9de5fb61029d448550b1667110cbae9ff5258056
danny <danny.bird@systemc.com>
2016-05-24 13:28:06 +0100

    first code
    will it work

---
+++ Code.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+using System.IO;
+using Microsoft.Deployment.WindowsInstaller;
+using System.Net;
+using System.Xml;
+using Ionic.Zip;
+using System.Diagnostics;
+
+public class Code
+{
+    
+}
